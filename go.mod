3aae1cce29fe450ea4b885d1d03316e96844b1f6
Fathurrohman <fathurrohmanrosyadi@gmail.com>
2024-08-22 02:18:34 +0700

    clean up v1

--- go.mod
+++
@@ -1,7 +0,0 @@
-module bo
-
-go 1.22
-
-require github.com/chzyer/readline v1.5.1
-
-require golang.org/x/sys v0.1.0 // indirect
--- object/return.go
+++
@@ -1,9 +0,0 @@
-package object
-
-// The Return struct represents a return object.
-type Return struct {
-	Value Object
-}
-
-func (r *Return) Type() ObjectType { return RETURN_OBJ }
-func (r *Return) Inspect() string  { return r.Value.Inspect() }
